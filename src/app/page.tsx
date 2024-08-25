1820418526e0267df3cd43cff19b7e2532abe548
CT254 <t_collins@robotconsulting.net>
2024-08-25 19:48:34 +0300

    chore: basic UI layout with mockdata
    what did you do?
    * basic UI layout with mockdata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,37 @@
+import Image from 'next/image';
 
 export default function HomePage() {
+
+  const mockUrls = [
+    'https://utfs.io/f/9ead65c2-e18e-4994-abf0-7893cdd18607-ex4y7s.jpg',
+    'https://utfs.io/f/d305356a-51bd-4ed6-ab5e-3ef66d8eb43e-8yiwgi.jpg',
+    'https://utfs.io/f/e83972da-02af-415a-b11b-01e69b2eab03-68d5zn.jpg',
+    'https://utfs.io/f/74689394-92f6-4d86-82c5-d3c3d2b180be-2a5e44.jpg',
+    'https://utfs.io/f/6084419c-165b-4860-ac7a-e67f6e19f0f6-heqaxb.jpg',
+
+  ];
+
+  const mockImages = mockUrls.map((url, index) => ({
+    id: index + 1,
+    url,
+  }));
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Currently under construction...
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48 p-2">
+            <Image
+              src={image.url}
+              alt="Image"
+              width={300}
+              height={300}
+              className="rounded-lg"
+            />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
